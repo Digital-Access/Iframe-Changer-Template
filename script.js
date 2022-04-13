0f2beed3f0677df2d6f93d1763a8c014e85e7941
Dominic <design@digitalaccess.tech>
2022-04-13 13:49:33 +0200

    progresstext

--- script.js
+++ script.js
@@ -4,20 +4,21 @@ const config = {
         "https://fast.wistia.net/embed/iframe/mbibneqh9f?seo=false&videoFoam=true",
         "https://fast.wistia.net/embed/iframe/rmv4no9y9d?seo=false&videoFoam=true",
         "https://fast.wistia.net/embed/iframe/9i2ybzrl5w?seo=false&videoFoam=true",
-        "https://fast.wistia.net/embed/iframe/6dygkgtgfh?seo=false&videoFoam=true",
-        "https://fast.wistia.net/embed/iframe/kvx4rlwmu3?seo=false&videoFoam=true"
+        "https://fast.wistia.net/embed/iframe/6dygkgtgfh?seo=false&videoFoam=true"
     ],
     next_button: "Next Lesson",
-    previous_button: "Previous Lesson"
+    previous_button: "Previous Lesson",
+    progress_text: "Video "
 }
-
 
 const nxtBtn = document.getElementById('nxtBtn');
 const prevBtn = document.getElementById('prevBtn');
 const video = document.getElementById('video');
 const progress = document.getElementById('progress');
-const total = document.getElementById('total')
+const total = document.getElementById('total');
+const progressText = document.getElementById('progressText');
 
+progressText.innerText = config.progress_text;
 total.textContent = config.videos.length;
 nxtBtn.textContent = config.next_button;
 prevBtn.textContent = config.previous_button;
