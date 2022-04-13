d7325ab0bdd1fce6e8008d53cc34547f6399221d
Dominic <design@digitalaccess.tech>
2022-04-13 16:17:40 +0200

    logic and visual update

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 const config = {
     videos: [
-        "https://fast.wistia.net/embed/iframe/gqpzyivli7?seo=false&videoFoam=true",
-        "https://fast.wistia.net/embed/iframe/mbibneqh9f?seo=false&videoFoam=true",
-        "https://fast.wistia.net/embed/iframe/rmv4no9y9d?seo=false&videoFoam=true",
-        "https://fast.wistia.net/embed/iframe/9i2ybzrl5w?seo=false&videoFoam=true",
-        "https://fast.wistia.net/embed/iframe/6dygkgtgfh?seo=false&videoFoam=true"
+        "https://fast.wistia.net/embed/iframe/rhgkngj4yv?seo=false&videoFoam=true",
+        "https://fast.wistia.net/embed/iframe/rhgkngj4yv?seo=false&videoFoam=true",
+        "https://fast.wistia.net/embed/iframe/rhgkngj4yv?seo=false&videoFoam=true"
     ],
-    next_button: "Next Lesson",
-    previous_button: "Previous Lesson",
+    next_button: "Next video",
+    previous_button: "Previous video",
     progress_text: "Video "
 }
 
@@ -17,6 +15,8 @@ const video = document.getElementById('video');
 const progress = document.getElementById('progress');
 const total = document.getElementById('total');
 const progressText = document.getElementById('progressText');
+const navCntrl = document.getElementById('navCntrl');
+const progressDetails = document.getElementById('progressDetails')
 
 progressText.innerText = config.progress_text;
 total.textContent = config.videos.length;
@@ -26,6 +26,8 @@ video.src = config.videos[0];
 
 let i = 0;
 progress.textContent = i + 1;
+prevBtn.style.display = 'none'
+navCntrl.style.justifyContent = 'flex-end'
 
 let max = config.videos.length;
 const nextVideo = () => {
@@ -33,7 +35,14 @@ const nextVideo = () => {
         i++
         video.src = config.videos[i];
     }
+
+    if(i === max -1){
+        nxtBtn.style.display = 'none'
+    }
+
     progress.textContent = i + 1;
+    prevBtn.style.display = 'flex'
+    navCntrl.style.justifyContent = 'space-between'
 }
 
 nxtBtn.addEventListener('click', nextVideo)
@@ -43,9 +52,24 @@ const prevVideo = () => {
     if (i != 0) {
         i--
         video.src = config.videos[i];
+        nxtBtn.style.display = 'flex';
+    } 
+
+    if(i === 0){
+        prevBtn.style.display = 'none'
+        navCntrl.style.justifyContent = 'flex-end'
     }
+
     progress.textContent = i + 1;
 }
 
-
 prevBtn.addEventListener('click', prevVideo)
+
+
+
+if (config.videos.length === 1 || config.videos[i] === "") {
+    nxtBtn.style.display = 'none'
+    prevBtn.style.display = 'none'
+    navCntrl.style.justifyContent = 'center'
+    navCntrl.style.padding = '1.25rem 0 1.25rem 0'
+}
